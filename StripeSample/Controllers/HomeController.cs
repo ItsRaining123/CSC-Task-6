6e25cdc9875f5cb7cecd617cd1158a137929fa4b
JOVIANLIM.17@ichat.sp.edu.sg <JOVIANLIM.17@ichat.sp.edu.sg>
2020-06-26 23:26:56 +0800

    Add project files.

---
+++ StripeSample/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System.Web.Mvc;
+
+namespace StripeSample.Controllers
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Microsoft.Ajax.Utilities;
+    using Models;
+    using Stripe;
+
+    public class HomeController : Controller
+    {
+        string key = "key";
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Charge()
+        {
+            ViewBag.Message = "Learn how to process payments with Stripe";
+
+            return View(new StripeChargeModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Charge(StripeChargeModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var chargeId = await ProcessPayment(model);
+            return View("Index");
+        }
+
+        
+
+
+        private async Task<string> ProcessPayment(StripeChargeModel model)
+        {
+            return await Task.Run(() =>
+            {
+                
+                var myCharge = new StripeChargeCreateOptions
+                {
+                    // convert the amount of £12.50 to pennies i.e. 1250
+                    Amount = (int)(model.Amount * 100),
+                    Currency = "gbp",
+                    Description = "Description for test charge",
+                    Source = new StripeSourceOptions{
+                        TokenId = model.Token
+                    }
+                };
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)768 | (SecurityProtocolType)3072;
+                var chargeService = new StripeChargeService("key");
+                var stripeCharge = chargeService.Create(myCharge);
+
+                return stripeCharge.Id;
+           });
+        }
+    }
+}
